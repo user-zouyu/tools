a206bb549a95302b14baa0628f61dd738f9d9a4c
YuZou <user.zouyu@foxmail.com>
2023-04-14 13:52:03 +0800

    feat: 添加文本显示

--- ScreenSharer/helper-ui/src/show/index.jsx
+++ ScreenSharer/helper-ui/src/show/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import {Image} from "antd";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import {vs2015} from "react-syntax-highlighter/src/styles/hljs/index.js";
+
 
 const Show = (props) => {
-    const index = props.index
-    const data = props.list[index];
-    if (index < 0) {
+    let item = {}
+    console.log(props)
+    if (props.id !== -1) {
+        for (let i = 0; i < props.list.length; i++) {
+            if (props.list[i].id === props.id) {
+                item = props.list[i]
+            }
+        }
+    }
+    if (props.id < 0) {
         return (
             <div style={{
                 display: "flex",
@@ -13,20 +23,36 @@ const Show = (props) => {
                 alignItems: "center",
                 fontSize: "30px"
             }}>
-                shift+s 切换到下一张照片
+                欢迎使用
             </div>
         )
     }
-    if (index >= props.list.length) {
+
+
+    if (item.type === "text") {
+        const data = JSON.parse(item.data)
+        return (
+            <div style={{
+                width: "calc(100%)",
+                minHeight: "100%",
+                display: "inline-block",
+                padding: "0 14px"
+            }}>
+                <SyntaxHighlighter language={data.language} style={vs2015}>
+                    {data.text}
+                </SyntaxHighlighter>
+            </div>
+        )
+    }
+
+    if (item.type === "image") {
         return (
             <div style={{
                 display: "flex",
-                marginTop: "30px",
                 justifyContent: "space-around",
-                alignItems: "center",
-                fontSize: "30px"
+                alignItems: "center"
             }}>
-                shift+w 切换到上一张照片
+                <Image style={{maxHeight: "100vh"}} src={item.data}/>
             </div>
         )
     }
@@ -34,12 +60,15 @@ const Show = (props) => {
     return (
         <div style={{
             display: "flex",
+            marginTop: "30px",
             justifyContent: "space-around",
-            alignItems: "center"
+            alignItems: "center",
+            fontSize: "30px"
         }}>
-            <Image style={{maxHeight: "100vh"}} src={data.url}/>
+            暂不支持该数据类型
         </div>
     )
+
 }
 
 export default Show;
